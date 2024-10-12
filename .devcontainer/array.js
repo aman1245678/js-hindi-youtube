e7d379ea5008263665102861fb88b88a4a1ed2dc
aman1245678 <gauravkashyap7600@gmail.com>
2024-10-12 11:23:35 +0000

    yeah

---
+++ .devcontainer/array.js
@@ -0,0 +1,56 @@
+//  const myarr = [0,1,2,3,4,5,6,]
+
+//  const myHeroes = ["Aman", "Kumar"]
+//  console.log(myarr[2]);
+
+
+//  myHeroes.push(6)
+//  console.log(myHeroes);
+ 
+//  myarr.pop()
+//  console.log(myarr);
+
+//  myarr.unshift(7)
+ 
+// console.log(myarr.indexOf(7));
+//  const newArr = myarr.join()
+
+//  console.log(myarr);
+//  console.log(typeof newArr);
+
+//  console.log("A", myarr);
+
+//  const myn1 = myarr.slice(1,3);
+//  console.log(myn1);
+//  console.log("B",myarr);
+
+//  const myn2  = myarr.splice(1,3)
+//  console.log("C",myarr);
+ 
+//  console.log(myn2);
+ 
+ 
+ const marvel_heroes = ["ironman","thor","spiderhan"]
+ const dc_heroes = ["superman","batman","awuaman"]
+
+ marvel_heroes.push(dc_heroes);
+
+//  console.log(marvel_heroes);
+//  console.log(marvel_heroes[3][1]);
+//  const Allheroes = marvel_heroes.concat(dc_heroes)
+//  console.log(marvel_heroes);
+//  const All_new_heroes = [...marvel_heroes, ...dc_heroes]
+//  console.log(All_new_heroes);
+
+//  const Anotherarr  = [1,2,3,4,[4,5,3],5,[7,4,6[3,4,[7,4]]]]
+//  const myrael = Anotherarr.flat(Infinity)
+//  console.log(myrael);
+
+//  console.log(Array.isArray("Aman"));
+//  console.log(Array.from("Aman"));
+//  console.log(Array.from({name: "Aman"}));
+//  let sc= 100
+//  let sc1 = 200
+//  let sc2 = 993
+//  console.log(Array.of(sc,sc1,sc2) );
+ 
