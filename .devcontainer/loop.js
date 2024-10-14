88d1f99ad3ac3e5aa7066f20b85c1d729df06e99
aman1245678 <gauravkashyap7600@gmail.com>
2024-10-14 14:10:03 +0000

    seve

--- .devcontainer/loop.js
+++ .devcontainer/loop.js
@@ -44,3 +44,77 @@
 // }
 
 
+// let index = 0
+// while (index <= 20) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 1
+    
+// }
+// let myArray = ["Aman", "Kumar","Safe"]
+
+// let arr = 0
+
+// while(arr < myArray.length ) {
+    
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+    
+// }
+
+// let score = 1
+
+// do{
+//     console.log(`score is ${score}`);
+//     score++
+    
+// } while(score <= 10)
+
+//for of
+
+const arr = [1,2,3,4,5,6]
+for (const num of arr){
+    console.log(num);
+    
+}
+const greeting = " hello world"
+for (const greet of greeting){
+    console.log(`each char is ${greet}`);
+    
+}
+//Maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA',"United state of america")
+map.set('fr',"France")
+map.set('fr',"France")
+//console.log(map);
+
+for (const [key,value] of map){
+    console.log(key ,':-',value);
+    
+}
+// const myobject = {
+//     'game1': 'NFS',
+//     'game2': 'kfs'
+// }
+// for (const [key , value] of myobject) {
+//     console.log(key,':-',value);
+    
+// }
+// const myobject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swif: "swift by apple"
+
+// }
+// for (const key in myobject){
+//     console.log(`${key} shortcut is for ${myobject[key]} `);
+    
+// }
+// const programming = ["js", "rb", "py" ,"java", "cpp"]
+
+// for (const key in programming){
+//     console.log(programming[key]);
+    
+// }
